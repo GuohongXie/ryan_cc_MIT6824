ec914ea7edea9d46e34a6c5f204d6b183a21994e
guohong <xieg678@gmail.com>
2023-07-03 16:01:05 +0800

    add files

---
+++ src/map_reduce/mr/coordinator.cc
@@ -0,0 +1,253 @@
+#include <unistd.h>  // ::sleep()
+
+//#include <iostream>
+#include <cstdio>
+#include <string>
+#include <unordered_map>
+#include <list>
+#include <mutex>
+//#include <condition_variable>
+//#include <any>
+#include <thread>
+//#include <chrono>
+
+#include "buttonrpc.hpp"
+
+#define MAP_TASK_TIMEOUT 3
+#define REDUCE_TASK_TIMEOUT 5
+
+class Coordinator {
+ public:
+  // std::any is a new feature in c++17
+  static void* WaitMapTask(void* arg);     //回收map的定时线程
+  static void* WaitReduceTask(void* arg);  //回收reduce的定时线程
+  static void* WaitTime(void* arg);        //用于定时的线程
+  Coordinator(
+      int map_num = 8,
+      int reduce_num = 8);  //带缺省值的有参构造，也可通过命令行传参指定，我偷懒少打两个数字直接放构造函数里
+  void GetAllFile(int argc, char* argv[]);  //从argv[]中获取待处理的文件名
+  int map_num() { return map_num_; }
+  int reduce_num() { return reduce_num_; }
+  std::string AssignTask();               //分配map任务的函数，RPC
+  int AssignReduceTask();            //分配reduce任务的函数，RPC
+  void SetMapStat(std::string file_name);  //设置特定map任务完成的函数，RPC
+  bool IsMapDone();                  //检验所有map任务是否完成，RPC
+  void SetReduceStat(int task_index);  //设置特定reduce任务完成的函数，RPC
+  void WaitMap(std::string file_name);
+  void WaitReduce(int reduce_idx);
+  bool Done();  //判断reduce任务是否已经完成
+  bool GetFinalStat() {  //所有任务是否完成，实际上reduce完成就完成了，有点小重复
+    return done_;
+  }
+
+ private:
+  std::mutex mutex_;
+  //std::condition_variable cv_;
+  bool done_;
+  std::list<std::string> input_file_name_list_;  //所有map任务的工作队列
+  int input_file_num_;  //从命令行读取到的文件总数
+  int map_num_;
+  int reduce_num_;
+  std::unordered_map<std::string, int> finished_map_task_;  //存放所有完成的map任务对应的文件名
+  std::unordered_map<int, int> finished_reduce_task_;  //存放所有完成的reduce任务对应的reduce编号
+  vector<int> reduce_index_;  //所有reduce任务的工作队列
+  vector<std::string> running_map_work_;  //正在处理的map任务，分配出去就加到这个队列，用于判断超时处理重发
+  int curr_map_index_;  //当前处理第几个map任务
+  int curr_reduce_index_;  //当前处理第几个reduce任务
+  vector<int> running_reduce_work_;  //正在处理的reduce任务，分配出去就加到这个队列，用于判断超时处理重发
+};
+
+Coordinator::Coordinator(int map_num, int reduce_num)
+    : done_(false), map_num_(map_num), reduce_num_(reduce_num) {
+  input_file_name_list_.clear();
+  finished_map_task_.clear();
+  finished_reduce_task_.clear();
+  running_map_work_.clear();
+  running_reduce_work_.clear();
+  curr_map_index_ = 0;
+  curr_reduce_index_ = 0;
+  if (map_num_ <= 0 || reduce_num_ <= 0) {
+    throw std::exception();
+  }
+  for (int i = 0; i < reduce_num; i++) {
+    reduce_index_.emplace_back(i);
+  }
+}
+
+/// @brief get all the source file names(std::string) into input_file_name_list_ 
+/// @param argc is the number of command line arguments
+/// @param argv is the value of command line arguments
+void Coordinator::GetAllFile(int argc, char* argv[]) {
+  for (int i = 1; i < argc; i++) {
+    input_file_name_list_.emplace_back(argv[i]);
+  }
+  input_file_num_ = argc - 1;
+}
+
+// map的worker只需要拿到对应的文件名就可以进行map
+std::string Coordinator::AssignTask() {
+  if (IsMapDone()) return "empty";
+  if (!input_file_name_list_.empty()) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    std::string task = input_file_name_list_.back();  //从工作队列取出一个待map的文件名
+    input_file_name_list_.pop_back();
+    lock.unlock();
+    WaitMap(task);  //调用waitMap将取出的任务加入正在运行的map任务队列并等待计时线程
+    return task;
+  }
+  return "empty";
+}
+
+
+void* Coordinator::WaitMapTask(void* arg) {
+  Coordinator* map = static_cast<Coordinator*>(arg);
+  std::thread tid([&]() {
+    char op = 'm';
+    WaitTime(&op);
+  });
+  tid.join();  // join方式回收实现超时后解除阻塞
+  std::unique_lock<std::mutex> lock(map->mutex_);
+  //若超时后在对应的hashmap中没有该map任务完成的记录，重新将该任务加入工作队列
+  if (!map->finished_map_task_.count(map->running_map_work_[map->curr_map_index_])) {
+    printf("file_name : %s is timeout\n", map->running_map_work_[map->curr_map_index_].c_str());
+    // char text[map->running_map_work_[map->curr_map_index_].size() + 1];
+    // strcpy(text, map->running_map_work_[map->curr_map_index_].c_str());
+    // printf("text is %s\n", text);
+    // 打印正常的，该线程结束后text就变成空字符串了
+    const std::string& text = map->running_map_work_[map->curr_map_index_];  //这钟方式加入list后不会变成空字符串
+    map->input_file_name_list_.push_back(text);
+    map->curr_map_index_++;
+    lock.unlock();
+    return nullptr;
+  }
+  printf("file_name : %s is finished at idx : %d\n", map->running_map_work_[map->curr_map_index_].c_str(), map->curr_map_index_);
+  ++(map->curr_map_index_);
+}
+
+
+void Coordinator::WaitMap(std::string file_name) {
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    running_map_work_.push_back(file_name);  //将分配出去的map任务加入正在运行的工作队列
+  }
+  std::thread t(&Coordinator::WaitMapTask, this);  //创建一个用于回收计时线程及处理超时逻辑的线程
+  t.detach();
+}
+
+
+//分map任务还是reduce任务进行不同时间计时的计时线程
+void* Coordinator::WaitTime(void* arg) {
+  char* op = static_cast<char*>(arg);
+  if (*op == 'm') {
+    ::sleep(MAP_TASK_TIMEOUT);
+  } else if (*op == 'r') {
+    ::sleep(REDUCE_TASK_TIMEOUT);
+  }
+}
+
+
+void* Coordinator::WaitReduceTask(void* arg) {
+  Coordinator* reduce = static_cast<Coordinator*>(arg);
+  void* status;
+  std::thread t([&]() {
+    char op = 'r';
+    WaitTime(&op);
+  });
+  t.join();
+  std::unique_lock<std::mutex> lock(reduce->mutex_);
+  //若超时后在对应的hashmap中没有该reduce任务完成的记录，将该任务重新加入工作队列
+  if (!reduce->finished_reduce_task_.count(
+          reduce->running_reduce_work_[reduce->curr_reduce_index_])) {
+    for (auto a : reduce->input_file_name_list_) {
+      printf(" before insert %s\n", a);
+    }
+    reduce->reduce_index_.emplace_back(reduce->running_reduce_work_[reduce->curr_reduce_index_]);
+    printf(" reduce%d is timeout\n", reduce->running_reduce_work_[reduce->curr_reduce_index_]);
+    (reduce->curr_reduce_index_)++;
+    for (auto a : reduce->input_file_name_list_) {
+      printf(" after insert %s\n", a);
+    }
+    lock.unlock();
+    return nullptr;
+  }
+  printf("%d reduce is completed\n", reduce->running_reduce_work_[reduce->curr_reduce_index_]);
+  reduce->curr_reduce_index_++; 
+}
+
+
+void Coordinator::WaitReduce(int reduce_idx) {
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    running_reduce_work_.push_back(reduce_idx);  //将分配出去的reduce任务加入正在运行的工作队列
+  }
+  std::thread t(&Coordinator::WaitReduceTask, this);  //创建一个用于回收计时线程及处理超时逻辑的线程
+  t.detach();
+}
+
+
+
+void Coordinator::SetMapStat(std::string file_name) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  finished_map_task_[file_name] = 1;  //通过worker的RPC调用修改map任务的完成状态
+  // printf("map task : %s is finished, maphash is %p\n", file_name.c_str(),
+  // &finished_map_task_);
+  return;
+}
+
+bool Coordinator::IsMapDone() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (finished_map_task_.size() !=
+      input_file_num_) {  //当统计map任务的hashmap大小达到文件数，map任务结束
+    return false;
+  }
+  return true;
+}
+
+int Coordinator::AssignReduceTask() {
+  if (Done()) return -1;
+  if (!reduce_index_.empty()) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    int reduce_idx = reduce_index_.back();  //取出reduce编号
+    reduce_index_.pop_back();
+    lock.unlock();
+    WaitReduce(
+        reduce_idx);  //调用waitReduce将取出的任务加入正在运行的reduce任务队列并等待计时线程
+    return reduce_idx;
+  }
+  return -1;
+}
+
+void Coordinator::SetReduceStat(int task_index) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  finished_reduce_task_[task_index] = 1;  //通过worker的RPC调用修改reduce任务的完成状态
+  // printf(" reduce task%d is finished, reducehash is %p\n", task_index,
+  // &finished_reduce_task_);
+  return;
+}
+
+bool Coordinator::Done() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  return finished_reduce_task_.size() == reduce_num_;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("missing parameter! The format is ./coordinator pg*.txt");
+    exit(-1);
+  }
+  // alarm(10);
+  buttonrpc server;
+  server.as_server(5555);
+  Coordinator coordinator(13, 9);
+  coordinator.GetAllFile(argc, argv);
+  server.bind("map_num", &Coordinator::map_num, &coordinator);
+  server.bind("reduce_num", &Coordinator::reduce_num, &coordinator);
+  server.bind("AssignTask", &Coordinator::AssignTask, &coordinator);
+  server.bind("SetMapStat", &Coordinator::SetMapStat, &coordinator);
+  server.bind("IsMapDone", &Coordinator::IsMapDone, &coordinator);
+  server.bind("AssignReduceTask", &Coordinator::AssignReduceTask, &coordinator);
+  server.bind("SetReduceStat", &Coordinator::SetReduceStat, &coordinator);
+  server.bind("Done", &Coordinator::Done, &coordinator);
+  server.run();
+  return 0;
+}
---
+++ src/map_reduce/mr/worker.cc
@@ -0,0 +1,466 @@
+#include <dirent.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cstdio>
+#include <string>
+#include <unordered_map>
+#include <list>
+#include <mutex>
+#include <condition_variable>
+//#include <any>
+#include <thread>
+
+#include "buttonrpc.hpp"
+
+
+//可能造成的bug，考虑write多次写，每次写1024用while读进buf
+// c_str()返回的是一个临时指针，值传递没事，但若涉及地址出错
+
+//定义实际处理map任务的数组，存放map任务号
+//(map任务大于总文件数时，多线程分配ID不一定分配到正好增序的任务号，如10个map任务，总共8个文件，可能就是0,1,2,4,5,7,8,9)
+struct KeyValue {
+  std::string key;
+  std::string value;
+};
+
+using MapFunc = std::vector<KeyValue> (*)(KeyValue);
+using ReduceFunc = std::vector<std::string> (*)(std::vector<KeyValue>, int);
+
+
+class Worker {
+ public:
+  //定义的两个函数指针用于动态加载动态库里的map和reduce函数
+  using MapFunc = std::vector<KeyValue> (*)(KeyValue);
+  using ReduceFunc = std::vector<std::string> (*)(std::vector<KeyValue>, int);
+  static MapFunc map_func;
+  static ReduceFunc reduce_func;
+  constexpr int MAX_REDUCE_NUM = 15;
+
+  Worker(int disabled_map_id,     //用于人为让特定map任务超时的Id
+         int disabled_reduce_id,  //用于人为让特定reduce任务超时的Id
+         int map_task_num,
+         int reduce_task_num) 
+      : disabled_map_id_(disabled_map_id),
+        disabled_reduce_id_(disabled_reduce_id),
+        map_task_num_(map_task_num),
+        reduce_task_num_(reduce_task_num) {}
+  Worker() 
+      : disabled_map_id_(0),
+        disabled_reduce_id_(0),
+        map_task_num_(0),
+        reduce_task_num_(0) {}
+ 
+  ~Worker() {}
+
+  void* MapWorker(void* arg);
+  void* ReduceWorker(void* arg);
+  void RemoveTmpFiles();
+  void RemoveOutputFiles();
+
+  void set_map_task_num(int num) { map_task_num_ = num; }
+  void set_reduce_task_num(int num) { reduce_task_num_ = num; }
+  int map_task_num() const { return map_task_num_; }
+  int reduce_task_num() const { return reduce_task_num_; }
+
+ private:
+  int Ihash(std::string str);  //对每个字符串求hash找到其对应要分配的reduce线程
+  void MyWrite(int fd, std::vector<std::string>& str);  //用于最后写入磁盘的函数，输出最终结果
+  KeyValue GetContent(const std::string& file_name);
+  std::vector<std::string> GetAllfile(std::string path, int op); //获取对应reduce编号的所有中间文件
+  std::vector<KeyValue> MyShuffle(int reduce_task_num);  // vector中每个元素的形式为"abc 11111";
+  void WriteKV(int fd, const KeyValue& kv);
+  void WriteInDisk(const std::vector<KeyValue>& kvs, int map_task_index);
+  std::vector<std::string> Split(std::string text, char op); //以char类型的op为分割拆分字符串
+  std::string Split(std::string text); //以char类型的op为分割拆分字符串
+
+  std::mutex mutex_;
+  std::condition_variable cond_;
+  int disabled_map_id_;     //用于人为让特定map任务超时的Id
+  int disabled_reduce_id_;  //用于人为让特定reduce任务超时的Id
+  int map_task_num_;  //由coordinator分配，通过main函数的rpc从coordinator获取
+  int reduce_task_num_;  //由coordinator分配，通过main函数的rpc从coordinator获取
+  static int map_id_;  //给每个线程分配任务的id，用于写中间文件的命名
+};
+
+int Worker::map_id_ = 0;
+
+//TODO:Ihash有什么用
+//对每个字符串求hash找到其对应要分配的reduce线程
+int Worker::Ihash(std::string str) {
+  int sum = 0;
+  for (int i = 0; i < str.size(); i++) {
+    sum += (str[i] - '0');  // TODO: not modern c++
+  }
+  return sum % reduce_task_num_;
+}
+
+
+/// @brief 删除所有写入中间值的临时文件
+/// @brief 之所以不用任何参数是因为临时文件的命名格式固定
+/// @brief 临时文件命名格式为"mr-i-j"
+/// @brief 其中i为map_task_index, j为reduce_task_index
+void Worker::RemoveTmpFiles() {
+  std::string tmp_file_path;
+  for (int i = 0; i < map_task_num_; i++) {
+    for (int j = 0; j < reduce_task_num_; j++) {
+      tmp_file_path = "mr-" + std::to_string(i) + "-" + std::to_string(j);
+      int ret = ::access(tmp_file_path.c_str(), F_OK);
+      if (ret == 0) ::remove(tmp_file_path.c_str()); // ret==0表示文件存在
+    }
+  }
+}
+
+
+/// @brief 取得  key:filename, value:content 的kv对作为map任务的输入
+/// @param file_name 就是map任务要处理的文件的名字
+/// @return key:filename, value:content 的kv对作为map任务的输入
+KeyValue Worker::GetContent(const std::string& file_name) {
+  int fd = ::open(file_name.c_str(), O_RDONLY);
+  int length = ::lseek(fd, 0, SEEK_END);
+  ::lseek(fd, 0, SEEK_SET);
+  char buf[length];  //之所以用 char bug[]是为了匹配系统调用read
+  ::bzero(buf, length);
+  int len = ::read(fd, buf, length);
+  if (len != length) {
+    ::perror("read");
+    ::exit(-1);
+  }
+  KeyValue kv;
+  kv.key = std::string(file_name);
+  kv.value = std::string(buf);
+  ::close(fd);
+  return kv;
+}
+
+
+/// @brief 将map任务产生的中间值写入临时文件
+/// @param fd 
+/// @param kv 
+void Worker::WriteKV(int fd, const KeyValue& kv) {
+  std::string tmp = kv.key + ",1 ";
+  int len = ::write(fd, tmp.c_str(), tmp.size());
+  if (len == -1) {
+    ::perror("write");
+    ::exit(-1);
+  }
+  ::close(fd);
+}
+
+
+/// @brief 创建每个map任务对应的不同reduce号的中间文件并调用 WriteKV 写入磁盘
+/// @param kvs map_func产生的输出 
+/// @param map_task_index 线程所对应的map_task_index，由MapWorker函数(线程启动函数)确定并传进参数于此 
+void Worker::WriteInDisk(const std::vector<KeyValue>& kvs, int map_task_index) {
+  for (const auto& kv : kvs) {
+    int reduce_task_index = Ihash(kv.key); //reduce_task_index由kv.key的Ihash决定
+    std::string path;
+    path = "mr-" + std::to_string(map_task_index) + "-" + std::to_string(reduce_task_index);
+    int ret = ::access(path.c_str(), F_OK);
+    if (ret == -1) {
+      int fd = ::open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0664);
+      WriteKV(fd, kv);
+    } else if (ret == 0) {
+      int fd = ::open(path.c_str(), O_WRONLY | O_APPEND);
+      WriteKV(fd, kv);
+    }
+  }
+}
+
+
+/// @brief 以char类型的op为分割拆分字符串
+/// @param text 
+/// @param op 
+/// @return 
+std::vector<std::string> Worker::Split(std::string text, char op) {
+  int n = text.size();
+  std::vector<std::string> str;
+  std::string tmp = "";
+  for (int i = 0; i < n; i++) {
+    if (text[i] != op) {
+      tmp += text[i];
+    } else {
+      if (tmp.size() != 0) str.push_back(tmp);
+      tmp = "";
+    }
+  }
+  return str;
+}
+
+//以逗号为分割拆分字符串
+std::string Worker::Split(std::string text) {
+  std::string tmp = "";
+  for (int i = 0; i < text.size(); i++) {
+    if (text[i] != ',') {
+      tmp += text[i];
+    } else
+      break;
+  }
+  return tmp;
+}
+
+
+/// @brief 获取对应reduce编号的所有中间文件
+/// @param directory 
+/// @param op 
+/// @return 
+std::vector<std::string> Worker::GetAllfile(std::string directory , int op) {
+  DIR* dir = ::opendir(directory.c_str());
+  std::vector<std::string> ret;
+  if (dir == nullptr) {
+    printf("[ERROR] %s is not a directory or not exist!", directory.c_str());
+    return ret;
+  }
+  struct dirent* entry;
+  while ((entry = ::readdir(dir)) != nullptr) {
+    int len = strlen(entry->d_name);
+    int oplen = std::to_string(op).size();
+    if (len - oplen < 5) continue;
+    std::string filename(entry->d_name);
+    if (!(filename[0] == 'm' && filename[1] == 'r' &&
+          filename[len - oplen - 1] == '-'))
+      continue;
+    std::string cmp_str = filename.substr(len - oplen, oplen);
+    if (cmp_str == std::to_string(op)) {
+      ret.push_back(entry->d_name);
+    }
+  }
+  ::closedir(dir);
+  return ret;
+}
+
+//对于一个ReduceTask，获取所有相关文件并将value的list以string写入vector
+// vector中每个元素的形式为"abc 11111";
+std::vector<KeyValue> Worker::MyShuffle(int reduce_task_num) {
+  std::string path;
+  std::vector<std::string> str;
+  str.clear();  //TODO: 是否多余
+  std::vector<std::string> filename = GetAllfile(".", reduce_task_num);
+  std::unordered_map<std::string, std::string> hash;
+  for (int i = 0; i < filename.size(); i++) {
+    std::string text = filename[i];
+    KeyValue kv = GetContent(text);
+    std::string context = kv.value;
+    std::vector<std::string> ret_str = Split(context, ' ');
+    str.insert(str.end(), ret_str.begin(), ret_str.end());
+  }
+  for (const auto& a : str) {
+    hash[Split(a)] += "1";
+  }
+  std::vector<KeyValue> ret_kvs;
+  KeyValue tmpKv;
+  for (const auto& a : hash) {
+    tmpKv.key = a.first;
+    tmpKv.value = a.second;
+    ret_kvs.push_back(tmpKv);
+  }
+  std::sort(ret_kvs.begin(), ret_kvs.end(),
+       [](KeyValue& kv1, KeyValue& kv2) { return kv1.key < kv2.key; });
+  return ret_kvs;
+}
+
+void* Worker::MapWorker(void* arg) {
+  // 1、初始化client连接用于后续RPC;获取自己唯一的MapTaskID
+  buttonrpc client;
+  client.as_client("127.0.0.1", 5555);
+  std::unique_lock<std::mutex> lock(mutex_);
+  int map_task_index = map_id_++;
+  lock.unlock();
+  bool ret = false;
+  while (1) {
+    // 2、通过RPC从Master获取任务
+    // client.set_timeout(10000);
+    ret = client.call<bool>("IsMapDone").val();
+    if (ret) {
+      cond_.notify_all(); // TODO: why?
+      return nullptr;
+    }
+    std::string task_tmp = client.call<std::string>("AssignTask").val();  //通过RPC返回值取得任务，在map中即为文件名
+    if (task_tmp == "empty") continue;
+    printf("%d get the task : %s\n", map_task_index, task_tmp.c_str());
+    lock.lock();
+    //std::unique_lock<std::mutex> lock(mutex);
+    //------------------------自己写的测试超时重转发的部分---------------------
+    //注：需要对应master所规定的map数量，因为是1，3，5被置为disabled，相当于第2，4，6个拿到任务的线程宕机
+    //若只分配两个map的worker，即0工作，1宕机，我设的超时时间比较长且是一个任务拿完在拿一个任务，所有1的任务超时后都会给到0，
+    //人为设置的crash线程，会导致超时，用于超时功能的测试
+    if (disabled_map_id_ == 1 || disabled_map_id_ == 3 || disabled_map_id_ == 5) {
+      disabled_map_id_++;
+      lock.unlock();
+      printf("%d recv task : %s  is stop\n", map_task_index, task_tmp.c_str());
+      while (1) {
+        sleep(2);
+      } //TODO: why?
+    } else {
+      disabled_map_id_++;
+    }
+    lock.unlock();
+    //------------------------自己写的测试超时重转发的部分---------------------
+
+    // 3、拆分任务，任务返回为文件path及map任务编号，将filename及content封装到kv的key及value中
+    const std::string task = task_tmp;
+    KeyValue kv = GetContent(task);
+
+    // 4、执行map函数，然后将中间值写入本地
+    std::vector<KeyValue> kvs = map_func(kv);
+    WriteInDisk(kvs, map_task_index);
+
+    // 5、发送RPC给master告知任务已完成
+    printf("%d finish the task : %s\n", map_task_index, task_tmp.c_str());
+    client.call<void>("SetMapStat", task_tmp);
+  }
+}
+
+//用于最后写入磁盘的函数，输出最终结果
+void Worker::MyWrite(int fd, std::vector<std::string>& str) {
+  int len = 0;
+  char buf[2];
+  sprintf(buf, "\n");
+  for (auto s : str) {
+    len = write(fd, s.c_str(), s.size());
+    write(fd, buf, strlen(buf));
+    if (len == -1) {
+      perror("write");
+      exit(-1);
+    }
+  }
+}
+
+void* Worker::ReduceWorker(void* arg) {
+  // RemoveTmpFiles();
+  buttonrpc client;
+  client.as_client("127.0.0.1", 5555);
+  bool ret = false;
+  while (1) {
+    //若工作完成直接退出reduce的worker线程
+    ret = client.call<bool>("Done").val();
+    if (ret) {
+      return nullptr;
+    }
+    int reduce_task_index = client.call<int>("AssignReduceTask").val();
+    if (reduce_task_index == -1) continue;
+    printf("%ld get the task%d\n", pthread_self(), reduce_task_index);
+    //TODO: mistake
+    //printf("%ld get the task%d\n", std::to_string(std::thread::get_id()).c_str(), reduce_task_index);
+    std::unique_lock<std::mutex> lock(mutex_);
+    //人为设置的crash线程，会导致超时，用于超时功能的测试
+    if (disabled_reduce_id_ == 1 || disabled_reduce_id_ == 3 ||
+        disabled_reduce_id_ == 5) {
+      disabled_reduce_id_++;
+      lock.unlock();
+      printf("recv task%d reduce_task_index is stop in %ld\n", reduce_task_index,
+             pthread_self());
+      while (1) {
+        sleep(2);
+      }
+    } else {
+      disabled_reduce_id_++;
+    }
+    lock.unlock();
+
+    //取得reduce任务，读取对应文件，shuffle后调用reduceFunc进行reduce处理
+    std::vector<KeyValue> kvs = MyShuffle(reduce_task_index);
+    std::vector<std::string> ret = reduce_func(kvs, reduce_task_index);
+    std::vector<std::string> str;
+    for (int i = 0; i < kvs.size(); i++) {
+      str.push_back(kvs[i].key + " " + ret[i]);
+    }
+    std::string filename = "mr-out-" + std::to_string(reduce_task_index);
+    int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0664);
+    MyWrite(fd, str);
+    close(fd);
+    printf("%ld finish the task%d\n", pthread_self(), reduce_task_index);
+    client.call<bool>(
+        "SetReduceStat",
+        reduce_task_index);  //最终文件写入磁盘并发起RPCcall修改reduce状态
+  }
+}
+
+//删除最终输出文件，用于程序第二次执行时清除上次保存的结果
+void Worker::RemoveOutputFiles() {
+  std::string path;
+  for (int i = 0; i < MAX_REDUCE_NUM; i++) {
+    path = "mr-out-" + std::to_string(i);
+    int ret = access(path.c_str(), F_OK);
+    if (ret == 0) remove(path.c_str());
+  }
+}
+
+
+#define LIB_CACULATE_PATH "./libmap_reduce.so"  //用于加载的动态库的路径
+
+std::mutex mutex1;
+std::condition_variable cond1;
+
+int main() {
+  Worker worker;
+
+
+  //运行时从动态库中加载map及reduce函数(根据实际需要的功能加载对应的Func)
+  void* handle = dlopen(LIB_CACULATE_PATH, RTLD_LAZY);
+  if (!handle) {
+    cerr << "Cannot open library: " << dlerror() << '\n';
+    exit(-1);
+  }
+  Worker::map_func = (MapFunc)dlsym(handle, "MapFunc");
+  if (!worker.map_func) {
+    cerr << "Cannot load symbol 'hello': " << dlerror() << '\n';
+    dlclose(handle);
+    exit(-1);
+  }
+  Worker::reduce_func = (ReduceFunc)dlsym(handle, "ReduceFunc");
+  if (!worker.reduce_func) {
+    cerr << "Cannot load symbol 'hello': " << dlerror() << '\n';
+    dlclose(handle);
+    exit(-1);
+  }
+
+  //作为RPC请求端
+  buttonrpc work_client;
+  work_client.as_client("127.0.0.1", 5555);
+  work_client.set_timeout(5000);
+  int map_task_num_tmp = work_client.call<int>("map_num").val();
+  int reduce_task_num_tmp = work_client.call<int>("reduce_num").val();
+  worker.set_map_task_num(map_task_num_tmp);
+  worker.set_reduce_task_num(reduce_task_num_tmp);
+  worker.RemoveTmpFiles();        //若有，则清理上次输出的中间文件
+  worker.RemoveOutputFiles();  //清理上次输出的最终文件
+
+  // 创建多个 map 及 reduce 的 worker 线程
+  std::vector<std::thread> map_threads(worker.map_task_num());
+  std::vector<std::thread> reduce_threads(worker.reduce_task_num());
+  for (int i = 0; i < map_task_num_tmp; i++) {
+    map_threads[i] = std::thread(&Worker::MapWorker, &worker);
+    map_threads[i].detach();
+  }
+  std::unique_lock<std::mutex> lock1(mutex1);
+  cond1.wait(lock1);
+  lock1.unlock();
+  for (int i = 0; i < reduce_task_num_tmp; i++) {
+    reduce_threads[i] = std::thread(&Worker::ReduceWorker,  &worker);
+    reduce_threads[i].detach();
+  }
+
+  // 循环检查任务是否完成
+  while (!work_client.call<bool>("Done").val()) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+
+  // 任务完成后清理中间文件，关闭打开的动态库，释放资源
+  worker.RemoveTmpFiles();
+  ::dlclose(handle);
+}
+
+  
